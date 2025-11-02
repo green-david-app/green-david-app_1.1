3cf1821f49682cdf62f7db14f98efec1ea4d2832
green-david-app <david.cafourek@gmail.com>
2025-11-02 21:14:44 +0100

    del

--- gd/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-api_bp = Blueprint('gd_api', __name__, url_prefix='/gd/api')
-
-def register_api(app):
-    from .employees import register_api as _reg
-    _reg(app, api_bp)
-    app.register_blueprint(api_bp)
