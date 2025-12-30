fbc08e66f148fb4753f498800bbe530dc35891eb
green-david-app <david.cafourek@gmail.com>
2025-12-30 18:43:06 +0100

    del all

--- patches/gd_api_alias_patch.py
+++
@@ -1,4 +0,0 @@
-# Alias so old "/gd/api/tasks" works exactly like "/api/tasks"
-@app.route("/gd/api/tasks", methods=["GET", "POST", "PATCH", "PUT", "DELETE"])
-def gd_api_tasks_alias():
-    return api_tasks()
